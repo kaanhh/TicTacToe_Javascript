12b3844633152e25bd9b09150a4e741fa65805a9
Kaan Ertugrul <kaan@kaan.com>
2024-08-02 12:54:30 +0200

    Gewinner Box Anzeige

--- js/main.js
+++ js/main.js
@@ -14,10 +14,15 @@ window.addEventListener('DOMContentLoaded', setzeZeichen);
 
 function setzeZeichen() {
     const cells = document.querySelectorAll('.cell');
+    const gewinnerText = document.getElementById('gewinner-text');
+
     
     const neustartButton = document.getElementById('neustart');
     neustartButton.addEventListener('click', ()=> {
         board.resetSpiel();
+        
+        gewinnerText.textContent = 'Noch kein Gewinner';
+        gewinnerText.style.display = 'none';
     });
 
     board.cells = cells;
@@ -30,7 +35,8 @@ function setzeZeichen() {
                 const gewinner = board.pruefeGewinn();
                 if (gewinner) {
                     console.log(`Spieler ${gewinner} hat gewonnen!`);
-                    // Hier können Sie zusätzliche Logik hinzufügen, z.B. das Spiel beenden oder die Felder farblich markieren
+                    gewinnerText.textContent = `Spieler ${gewinner} hat gewonnen!`;
+                    gewinnerText.style.display = 'block';
                     return;
                 }
 
